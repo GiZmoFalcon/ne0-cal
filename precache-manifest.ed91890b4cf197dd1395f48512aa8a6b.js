e4cd83d4b537cf25dcd9182bf4758e6e618603c9
Sourabh <darojisourabh@ymail.com>
2020-09-30 01:32:50 +0530

    Updates

---
+++ precache-manifest.ed91890b4cf197dd1395f48512aa8a6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7d20433756ffd16ba54e1fe9441050b",
+    "url": "/neo-calc/index.html"
+  },
+  {
+    "revision": "a3582fc4fa70cbd065bd",
+    "url": "/neo-calc/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "9dab650c720868b57030",
+    "url": "/neo-calc/static/css/main.30c95cda.chunk.css"
+  },
+  {
+    "revision": "a3582fc4fa70cbd065bd",
+    "url": "/neo-calc/static/js/2.def40ae9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/neo-calc/static/js/2.def40ae9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dab650c720868b57030",
+    "url": "/neo-calc/static/js/main.ca0b0e62.chunk.js"
+  },
+  {
+    "revision": "fa1b74a4d4089c23b567",
+    "url": "/neo-calc/static/js/runtime-main.16d1d6a0.js"
+  }
+]);
